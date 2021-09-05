e86d4cd7db33ed35a4fa570849e813f31775e826
Simon Ågren <simon.agren@gmail.com>
2021-09-06 00:09:37 +0200

    add time to pin function for toggling output (LED) pins

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+const (
+	hourPinOffset = 12
+)
+
 func main() {
 	// check the time
 	// round down to nearest
@@ -15,9 +19,9 @@ func main() {
 
 }
 
-func timeToString(t time.Time) string {
-	h := t.Hour()
-	m := t.Minute()
+func enumerate(t time.Time) (h, m int) {
+	h = t.Hour()
+	minutes := t.Minute()
 
 	// 12 hour clock
 	if h > 11 {
@@ -25,19 +29,35 @@ func timeToString(t time.Time) string {
 	}
 
 	// refer to next hour for all "to" times
-	if m >= 25 {
-		h += 1
+	if minutes >= 25 {
+		h = (h + 1) % 12
 	}
 
-	fives := m / 5
+	m = minutes / 5
+	return
+}
 
-	fmt.Println(fives)
+func timeToString(t time.Time) string {
+	hours, fives := enumerate(t)
 
 	if fives == 0 {
-		return fmt.Sprintf("%v", hText[h])
+		return fmt.Sprintf("%v", hText[hours])
 	}
 
-	return fmt.Sprintf("%v %v", mText[fives], hText[h])
+	return fmt.Sprintf("%v %v", mText[fives], hText[hours])
+}
+
+func timeToPinOut(t time.Time) (out int64) {
+	h, fives := enumerate(t)
+
+	for _, elem := range mPinOut[fives] {
+		out = out | 1<<(elem)
+	}
+
+	out = out | 1<<(h+hourPinOffset)
+
+	return
+
 }
 
 var mText = map[int]string{
@@ -67,5 +87,18 @@ var hText = map[int]string{
 	9:  "nio",
 	10: "tio",
 	11: "elva",
-	12: "tolv",
+}
+
+var mPinOut = map[int][]int{
+	1:  {2, 7},    // fem över
+	2:  {3, 7},    // tio över
+	3:  {4, 7},    // kvart över
+	4:  {5, 7},    // tjugo över
+	5:  {2, 8, 6}, // fem i halv
+	6:  {6},       // halv
+	7:  {2, 7, 6}, // fem över halv
+	8:  {5, 11},   // tjugo i
+	9:  {4, 10},   //kvart i
+	10: {3, 9},    // tio i
+	11: {2, 8},    // fem i
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"testing"
 	"time"
 
@@ -24,6 +25,7 @@ func TestTimeToString(t *testing.T) {
 		{input: "00:42AM", want: "tjugo i ett"},
 		{input: "00:53AM", want: "tio i ett"},
 		{input: "00:59AM", want: "fem i ett"},
+		{input: "11:59PM", want: "fem i tolv"},
 	}
 
 	for _, tc := range tests {
@@ -53,5 +55,39 @@ func TestTimeToString(t *testing.T) {
 
 		assert.Equal(t, s1, s2)
 	}
+}
 
+func TestTimeToPint(t *testing.T) {
+	type test struct {
+		input string // time i time.Kitchen format
+		want  string
+	}
+	tests := []test{
+		{input: "00:00AM", want: "1000000000000"},
+		{input: "00:06AM", want: "1000010000100"},
+		{input: "00:12AM", want: "1000010001000"},
+		{input: "00:23AM", want: "1000010100000"},
+		{input: "00:29AM", want: "10000101000100"},
+		{input: "00:30AM", want: "10000001000000"},
+		{input: "00:36AM", want: "10000011000100"},
+		{input: "00:42AM", want: "10100000100000"},
+		{input: "00:53AM", want: "10001000001000"},
+		{input: "00:59AM", want: "10000100000100"},
+	}
+
+	for _, tc := range tests {
+		time, err := time.Parse(time.Kitchen, tc.input)
+		if err != nil {
+			t.Fatal()
+		}
+
+		pins := timeToPinOut(time)
+		pinsString := strconv.FormatInt(pins, 2)
+		if err != nil {
+			t.Fatal()
+		}
+
+		assert.Equal(t, tc.want, pinsString)
+
+	}
 }
